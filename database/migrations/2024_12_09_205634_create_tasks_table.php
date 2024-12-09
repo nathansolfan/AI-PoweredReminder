a4660f390ff1a9071483b646672c8e1797e17cf1
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-09 21:01:53 +0000

    upate migration

--- database/migrations/2024_12_09_205634_create_tasks_table.php
+++ database/migrations/2024_12_09_205634_create_tasks_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'completed'])->default('pending');
+            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
+            $table->dateTime('deadling')->nullable();
             $table->timestamps();
         });
     }
