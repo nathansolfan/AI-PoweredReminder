4326d9dbcf0334a735b547920c5003d51dbb7c28
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-09 21:02:57 +0000

    update model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'priority',
+        'deadline'
+    ];
 }
