cea91efd188c6cd0f95fde067549eceb923e464c
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-10 20:06:36 +0000

    updates

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -58,24 +58,37 @@ class TaskController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Task $task)
     {
-        //
+        // 5️⃣
+        return view('tasks.edit', ['task' => $task]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Task $task)
     {
-        //
+        // 6️⃣
+        $validated = $request->validate([
+            'title' => 'required',
+            'description' => 'nullable',
+            'status' => 'required|in:pending, completed',
+            'priority' => 'required|in:low,medium,high',
+            'deadline' => 'nullable|date',
+        ]);
+
+        $task->update($validated);
+        return redirect()->route('tasks.index')->with('success', 'Task updated');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        //7️⃣
+        $task->delete();
+        return redirect()->route('tasks.index')->with('success', 'Task deleted');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
