4436db33cdac36f2d467602ca3f58bc15ffcdc26
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-12 19:45:59 +0000

    cleaning

--- app/helpers.php
+++ app/helpers.php
@@ -10,20 +10,20 @@ if (!function_exists('generateAIDescription')) {
      * @param string|null $deadline
      * @return string
      */
-
-     function generateAIDescription(string $taskTitle, ?string $deadline = null): string
-     {
+    function generateAIDescription(string $taskTitle, ?string $deadline = null): string
+    {
         $client = \OpenAI::client(config('services.openai.api_key'));
 
-        $prompt = "Here is a taskL:  '{$taskTitle}'. The deadline is: '{$deadline}'.Generate a creative and conside description for this task.";
+        $prompt = "Here is a task: '{$taskTitle}'. The deadline is: '{$deadline}'. Generate a creative and concise description for this task.";
 
         $response = $client->chat()->create([
             'model' => 'gpt-3.5-turbo',
             'messages' => [
-                ['role' => 'system', 'content' => 'You are a productiviy assistant who generates task description.'],
+                ['role' => 'system', 'content' => 'You are a productivity assistant who generates task descriptions.'],
                 ['role' => 'user', 'content' => $prompt],
             ],
         ]);
+
         return $response->choices[0]['message']['content'];
-     }
+    }
 }
