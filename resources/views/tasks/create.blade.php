93b629dedeb1b2ac4ee2278690210dd2b24a3b09
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-14 12:29:12 +0000

    updated create form

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -54,6 +54,14 @@
                             <input type="date" name="deadline" id="deadline" class="block w-full mt-1 rounded-md shadow-sm border-gray-300 dark:bg-gray-700 dark:border-gray-600" value="{{ old('deadline') }}" />
                         </div>
 
+                        <div>
+                            <label for="category" class="block font-medium text-sm text-gray-700">Category</label>
+                            <select name="category" id="category" class="block w-full mt-1 rounded-md shadow-sm">
+                                <option value="Work" {{old('category') == 'Work' ? 'selected' : ''}}>Work</option>
+                                <option value="Personal" {{old('category') == 'Personal' ? 'selected' : ''}}>Personal</option>
+                            </select>
+                        </div>
+
                         <div class="flex justify-end">
                             <button type="submit" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-white uppercase tracking-widest hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Create Task</button>
                         </div>
