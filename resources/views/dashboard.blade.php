b3a1ef867b48d8a535e9e225c0c4ec8169bfbf07
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-22 20:29:27 +0000

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,9 +4,11 @@
     </x-slot>
 
 
-        <div class="mt-8">
+    <div class="flex items-center justify-center mt-8">
+        <div class="space-x-4">
             <a href="{{ route('tasks.index') }}" class="bg-blue-500 text-white px-4 py-2 rounded">View Tasks</a>
             <a href="{{ route('overview') }}" class="bg-green-500 text-white px-4 py-2 rounded">View Overview</a>
         </div>
+    </div>
     </div>
 </x-app-layout>
