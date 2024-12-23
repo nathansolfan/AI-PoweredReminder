1529d7e41fe8b29b7f36c70231291a006176dfcf
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-23 19:03:35 +0000

    readded create button on index

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -25,7 +25,13 @@
                                 Search
                             </button>
                         </form>
+
+                        <a href="{{ route('tasks.create') }}"
+                            class="ml-4 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">
+                            Create Task
+                        </a>
                     </div>
+
 
                     <!-- Task List -->
                     @if ($tasks->isEmpty())
