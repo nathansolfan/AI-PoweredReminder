2a30c6eeb91ce666b3205c65b44a2aafd945e340
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-25 23:55:35 +0000

    corrected migration file

--- database/migrations/2024_12_09_205634_create_tasks_table.php
+++ database/migrations/2024_12_09_205634_create_tasks_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->date('deadline')->nullable();
             // new column
             $table->string('attachment')->nullable();
-            $table->foreignId('parent_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('parent_id')->nullable()->constrained('tasks')->onDelete('cascade'); // Self-referencing
             $table->timestamps();
         });
     }
