fd277686724768adb98318a8d74bd350160d3804
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-27 23:58:44 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/tasks/filter', [TaskController::class, 'filter'])->name('tasks.filt
 
 Route::resource('tasks', TaskController::class);
 Route::patch('/tasks/{task}/toggle-status', [TaskController::class, 'toggleStatus'])->name('tasks.toggleStatus');
+
 Route::get('/overview', [TaskController::class, 'overview'])->name('overview');
 Route::get('/tasks{task}/delete-attachment', [TaskController::class, 'deleteAttachment'])->name('tasks.deleteAttachment');
 Route::get('/notifications', [TaskController::class, 'notifications'])->name('notifications');
