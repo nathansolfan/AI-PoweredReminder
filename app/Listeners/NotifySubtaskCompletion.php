d252d2d14c085ffbb688c004fc947d92442248a6
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-01-02 23:30:43 +0000

    update

--- app/Listeners/NotifySubtaskCompletion.php
+++ app/Listeners/NotifySubtaskCompletion.php
@@ -2,7 +2,9 @@
 
 namespace App\Listeners;
 
+use App\Events\SubtaskCompleted;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
 use Illuminate\Queue\InteractsWithQueue;
 
 class NotifySubtaskCompletion
@@ -20,6 +22,8 @@ class NotifySubtaskCompletion
      */
     public function handle(object $event): void
     {
-        //
+        // Send a notification to the user
+        $user = $event->subtask->task->user();
+        Notification::send($user, new SubtaskCompleted($event->subtask));
     }
 }
